650275375c563e51b2b4e33a8efe91fe07af1349
Hemanshu <hemanshu.mahajan@gmail.com>
2021-09-27 11:27:02 +0530

    added font

--- App.js
+++ App.js
@@ -1,21 +1,40 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+import { Provider as PaperProvider } from "react-native-paper";
+import AppLoading from "expo-app-loading";
+import {
+  useFonts,
+  Nunito_900Black as nunitoBlack,
+  Nunito_300Light as nunitoLight,
+  Nunito_600SemiBold as nunitoSemiBold,
+} from "@expo-google-fonts/nunito";
+
+import theme from "./src/theme";
 
 export default function App() {
+  let [fontsLoaded] = useFonts({
+    nunitoBlack,
+    nunitoSemiBold,
+    nunitoLight,
+  });
+  if (!fontsLoaded) {
+    return <AppLoading />;
+  }
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <PaperProvider theme={theme}>
+      <View style={styles.container}>
+        <Text style={{ fontFamily: "nunitoBlack" }}>
+          Open up App.js to start working on your app!
+        </Text>
+      </View>
+    </PaperProvider>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
